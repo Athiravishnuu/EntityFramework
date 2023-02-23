84b785ba6fc7fc2a33694d9b77e779664ac4f41a
Athira Parameswaran s <athira007p@gmail.com>
2023-02-23 14:15:03 +0530

    Demodb added

---
+++ EntityFramework/DbContext/DemoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramework.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Net;
+
+namespace EntityFramework.Db
+{  
+       public class DemoDbContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Address> Address { get; set; }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+
+                var path = "Server=localhost\\SQLEXPRESS;Database=ZDB2;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true";
+                optionsBuilder.UseSqlServer(path);
+                optionsBuilder.UseSqlServer(path);
+                base.OnConfiguring(optionsBuilder);
+
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+    
+}
---
+++ EntityFramework/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace EntityFramework.Model
+{
+    public class Student
+    {
+         {
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public string Phonenumber { get; set; }
+        public virtual Address Address { get; set; }
+
+    }
+}
+}
